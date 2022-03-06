6ced697ff43344548ffa32a6dd4ec32fc4c72b13
Vikki Wong <viwo4708@colorado.edu>
2022-03-05 19:58:50 -0700

    finished popups in discover

--- homepage/libandsearch/index.js
+++ homepage/libandsearch/index.js
@@ -3,5 +3,39 @@ function myFunction() {
     var map = document.getElementById("popupmap");
     popup.classList.toggle("show");
     map.classList.toggle("show");
-  }
+}
   
+function func2() {
+    var popup = document.getElementById("myPopup2");
+    var map = document.getElementById("popupmap2");
+    popup.classList.toggle("show");
+    map.classList.toggle("show");
+}
+
+function func3() {
+    var popup = document.getElementById("myPopup3");
+    var map = document.getElementById("popupmap3");
+    popup.classList.toggle("show");
+    map.classList.toggle("show");
+}
+
+function func4() {
+    var popup = document.getElementById("myPopup4");
+    var map = document.getElementById("popupmap4");
+    popup.classList.toggle("show");
+    map.classList.toggle("show");
+}
+
+function func5() {
+    var popup = document.getElementById("myPopup5");
+    var map = document.getElementById("popupmap5");
+    popup.classList.toggle("show");
+    map.classList.toggle("show");
+}
+
+function func6() {
+    var popup = document.getElementById("myPopup6");
+    var map = document.getElementById("popupmap6");
+    popup.classList.toggle("show");
+    map.classList.toggle("show");
+}
